064cd018eed82e68137a2f04e000e6045d47d0fa
GustavoHerrera11 <11gustavoherrera@gmail.com>
2019-07-03 13:17:50 -0600

    Terminado

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
-
+// Modulo Clima
 var Clima = angular.module("Clima",[])
-
+// Controlador Weather
 Clima.controller("Wheater",function($rootScope, $scope, $http){
-
-	$scope.Tempreatura=[];
+	// HTTP
+	$http({
+		method:"GET",
+		url:"http://api.openweathermap.org/data/2.5/weather?q=Republic of Guatemala&units=metric&lang=es&APPID=9591c913209303ea1fa78ed8a9ae5d12"
+	}).then(function callbaksuccess(snapshot){
+		$scope.Temperatura=[];
+		$scope.Temperatura.push(snapshot)
+	})
 
 	$http({
 		method:"GET",
-		url:"http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=9591c913209303ea1fa78ed8a9ae5d12"
-	}).then(function (snapshot){
-		console.log(snapshot.data.name)
+		url: "http://api.openweathermap.org/data/2.5/weather?q=Departamento de Santa Rosa&units=metric&lang=es&APPID=9591c913209303ea1fa78ed8a9ae5d12"
+	}).then(function callbaksuccess(Santa){
+		console.log(Santa)
+		$scope.San=[];
+		$scope.San.push(Santa)
+	})
+
+	$http({
+		method:"GET",
+		url: "http://api.openweathermap.org/data/2.5/weather?q=Ojo de Agua&units=metric&lang=es&APPID=9591c913209303ea1fa78ed8a9ae5d12"
+	}).then(function callbaksuccess(Ojo){
+		console.log(Ojo)
+		$scope.de=[];
+		$scope.de.push(Ojo)
 	})
 })
